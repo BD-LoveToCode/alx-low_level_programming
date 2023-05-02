bcca3dda7b59b34759f62c6ebc891f5655c4637b
Dorcas Bamisile <dorcasbamisile2@gmail.com>
2023-05-02 06:41:10 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,26 @@
 
 int main(void)
 {
-	times_table();
-	return (0);
+	int num, multiply, product;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (multiply = 1; multiply <= 9; multiply++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = num * multiply;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) * '0');
+
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
