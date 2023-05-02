6eae91040d3c013663a660092c749ba07ade6b8c
Dorcas Bamisile <dorcasbamisile2@gmail.com>
2023-05-02 07:25:20 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,16 @@
 
 void print_to_98(int n)
 {
-	int x, y;
-
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (x = n; x <= 98; x++)
-		{
-			if (x != 98)
-				printf("%d, ", x);
-			else if (x == 98)
-				printf("%d\n", x);
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n)	}
 	}
-	else if (n >= 98)
+	else
 	{
-		for (y = n; y >= 98; y++)
-		{
-			if (y != 98)
-				printf("%d, ", y);
-			else if (y == 98)
-				printf("%d\n", y);
-		}
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
