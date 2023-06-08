cb4b6afdb5637e2c8b07c3c644cbc730d30aae2c
BD-LoveToCode <dorcasbamisile2@gmail.com>
2023-06-08 16:54:17 -0700

    commit

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c
+ * @c: Character
+ * Return: 1, else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
