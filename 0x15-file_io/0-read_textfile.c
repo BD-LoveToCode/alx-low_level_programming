7c889f4bedf3baa595b3f374ec60351e172924bf
BD-LoveToCode <dorcasbamisile2@gmail.com>
2023-06-13 16:18:17 -0700

    read file & create file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,36 @@
-#include "main.h" #include <stdlib.h>
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile- Read text file
- * @filename: file read
- * @letters: letters read
- * Return: return w, else 0
+ * read_textfile - reads text file
+ * @filename: filename
+ * @letters: letters
+ * Return: return letters no, else 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
 
 	free(buf);
-	close(fd);
-	return (w);
+
+	return (nwr);
 }
